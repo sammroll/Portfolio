ac810e365924f61ef1f8a01c310f37f7c3c49105
Samantha Roldan <143849870+sammroll@users.noreply.github.com>
2024-07-05 00:40:34 -0500

    Add files via upload

---
+++ CoverLetter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JoceApp.Pages
+{
+    public class CoverLetterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
